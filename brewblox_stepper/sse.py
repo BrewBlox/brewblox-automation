da4369380453e3a736b25704dac8e8f362be7ef2
Bob Steers <steers.bob@gmail.com>
2019-09-13 13:43:17 +0200

    implement SSE statuses

---
+++ brewblox_stepper/sse.py
@@ -0,0 +1,133 @@
+"""
+SSE API
+"""
+
+import asyncio
+import json
+import weakref
+from typing import Set
+
+from aiohttp import hdrs, web
+from aiohttp_sse import sse_response
+from brewblox_service import brewblox_logger, features, scheduler, strex
+
+from brewblox_stepper import store
+
+PUBLISH_INTERVAL_S = 5
+PUBLISH_TIMEOUT_S = 5
+
+LOGGER = brewblox_logger(__name__)
+routes = web.RouteTableDef()
+
+
+def get_publisher(app: web.Application):
+    return features.get(app, SSEPublisher)
+
+
+def setup(app: web.Application):
+    app.router.add_routes(routes)
+    features.add(app, SSEPublisher(app))
+
+
+class SSEPublisher(features.ServiceFeature):
+
+    def __init__(self, app: web.Application):
+        super().__init__(app)
+        self._task: asyncio.Task = None
+        self._queues: Set[asyncio.Queue] = weakref.WeakSet()
+        self._current = None
+
+    def __str__(self):
+        return f'{type(self).__name__}'
+
+    async def subscribe(self, queue: asyncio.Queue):
+        self._queues.add(queue)
+        try:
+            if self._current is None:
+                self._current = await store.get_runtime_store(self.app).all_statuses()
+            await queue.put(self._current)
+
+        except asyncio.CancelledError:  # pragma: no cover
+            raise
+
+        except Exception as ex:
+            LOGGER.info(f'Initial subscription push failed: {strex(ex)}')
+
+    async def startup(self, app: web.Application):
+        await self.shutdown(app)
+        self._task = await scheduler.create_task(app, self._broadcast())
+
+    async def before_shutdown(self, _):
+        for queue in self._queues:
+            await queue.put(asyncio.CancelledError())
+
+    async def shutdown(self, _):
+        await scheduler.cancel_task(self.app, self._task)
+        self._task = None
+
+    async def _broadcast(self):
+        LOGGER.info(f'Starting {self}')
+
+        try:
+            runtime_store: store.RuntimeStore = store.get_runtime_store(self.app)
+
+        except Exception as ex:  # pragma: no cover
+            LOGGER.error(strex(ex), exc_info=True)
+            raise ex
+
+        while True:
+            try:
+                await asyncio.sleep(PUBLISH_INTERVAL_S)
+
+                if not self._queues:
+                    self._current = None
+                    continue
+
+                self._current = await runtime_store.all_statuses()
+                coros = [q.put(self._current) for q in self._queues]
+                await asyncio.wait_for(asyncio.gather(*coros, return_exceptions=True), PUBLISH_TIMEOUT_S)
+
+            except asyncio.CancelledError:
+                break
+
+            except Exception as ex:
+                self._current = None
+                LOGGER.error(f'{self} encountered an error: {strex(ex)}')
+
+
+def _cors_headers(request):
+    return {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods':
+        request.headers.get('Access-Control-Request-Method', ','.join(hdrs.METH_ALL)),
+        'Access-Control-Allow-Headers':
+        request.headers.get('Access-Control-Request-Headers', '*'),
+        'Access-Control-Allow-Credentials': 'true',
+    }
+
+
+@routes.get('/sse/status')
+async def subscribe(request: web.Request) -> web.Response:
+    """
+    ---
+    summary: Periodically pushes all active objects
+    tags:
+    - Stepper
+    - Process
+    operationId: process.sse
+    produces:
+    - application/json
+    """
+    async with sse_response(request, headers=_cors_headers(request)) as resp:
+        publisher: SSEPublisher = get_publisher(request.app)
+        queue = asyncio.Queue()
+        await publisher.subscribe(queue)
+
+        while True:
+            data = await queue.get()
+            if isinstance(data, Exception):
+                raise data
+            await resp.send(json.dumps(data))
+
+    # Note: we don't ever expect to return the response
+    # Either the client cancels the request, or an exception is raised by publisher
---
+++ test/test_sse.py
@@ -0,0 +1,119 @@
+"""
+Tests brewblox_stepper.sse
+"""
+
+import asyncio
+import json
+
+import pytest
+from aiohttp.client_exceptions import ContentTypeError
+from asynctest import CoroutineMock
+from brewblox_service import http_client, scheduler
+
+from brewblox_stepper import api, sse, store
+
+TESTED = sse.__name__
+
+
+async def response(request, status=200):
+    retv = await request
+    assert retv.status == status
+    try:
+        return await retv.json()
+    except ContentTypeError:
+        return await retv.text()
+
+
+@pytest.fixture(autouse=True)
+async def conditions_mock(mocker):
+    m = mocker.patch(store.__name__ + '.conditions.check', CoroutineMock(return_value=False))
+    return m
+
+
+@pytest.fixture
+async def publisher(app):
+    return sse.get_publisher(app)
+
+
+@pytest.fixture
+async def short_interval(mocker):
+    mocker.patch(TESTED + '.PUBLISH_INTERVAL_S', 0.001)
+
+
+@pytest.fixture
+async def app(app, loop):
+    http_client.setup(app)
+    scheduler.setup(app)
+    store.setup(app)
+    api.setup(app)
+    sse.setup(app)
+
+    return app
+
+
+async def test_immediate_response(app, client, publisher, process):
+    await response(client.post('/process', json=process))
+    await response(client.post('/start/test-process'))
+
+    q1, q2 = asyncio.Queue(), asyncio.Queue()
+    await publisher.subscribe(q1)
+    await publisher.subscribe(q2)
+    statuses = await response(client.get('/status'))
+    assert q1.get_nowait() == statuses
+    assert q2.get_nowait() == statuses
+
+
+async def test_empty_response(app, client, publisher):
+    q = asyncio.Queue()
+    await publisher.subscribe(q)
+    assert q.get_nowait() == []
+
+
+async def test_error_response(conditions_mock, app, client, publisher, process):
+    await response(client.post('/process', json=process))
+    await response(client.post('/start/test-process'))
+
+    conditions_mock.side_effect = RuntimeError
+    q = asyncio.Queue()
+    await publisher.subscribe(q)
+
+    with pytest.raises(asyncio.QueueEmpty):
+        q.get_nowait()
+
+
+async def test_sse_response(app, client, process):
+    async with client.get('/sse/status') as resp:
+        expected = 'data: []'
+        chunk = await resp.content.read(len(expected))
+        assert chunk.decode() == expected
+
+
+async def test_sse_updates(short_interval, app, client, process):
+    async with client.get('/sse/status') as resp:
+        expected = 'data: []\r\n\r\n'
+
+        chunk = await resp.content.read(len(expected))
+        assert chunk.decode() == expected
+
+        chunk = await resp.content.read(len(expected))
+        assert chunk.decode() == expected
+
+    await response(client.post('/process', json=process))
+    await response(client.post('/start/test-process'))
+    statuses = await response(client.get('/status'))
+
+    async with client.get('/sse/status') as resp:
+        expected = f'data: {json.dumps(statuses)}'
+        chunk = await resp.content.read(len(expected))
+        assert chunk.decode() == expected
+
+
+async def test_sse_error(short_interval, app, client, process, conditions_mock):
+    await response(client.post('/process', json=process))
+    await response(client.post('/start/test-process'))
+
+    conditions_mock.side_effect = RuntimeError
+
+    async with client.get('/sse/status') as resp:
+        with pytest.raises(asyncio.TimeoutError):
+            await asyncio.wait_for(resp.content.read(1), 0.1)
