776fa1b9588d14dcbd9f77da9271581d697a0b33
Bob Steers <steers.bob@gmail.com>
2020-05-18 15:04:37 +0200

    Add webhook action

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,5 +1,4 @@
 import { ActionImpl, ConditionImpl } from '../types';
-import AlwaysFalse from './always-false';
 import BlockPatch from './block-patch';
 import BlockValue from './block-value';
 import TaskCreate from './task-create';
@@ -7,15 +6,16 @@ import TaskStatus from './task-status';
 import TimeAbsolute from './time-absolute';
 import TimeElapsed from './time-elapsed';
 import { ActionHandler, ConditionHandler } from './types';
+import Webhook from './webhook';
 
 export const actionHandlers: Record<ActionImpl['type'], ActionHandler> = {
   BlockPatch,
   TaskCreate,
+  Webhook,
 };
 
 export const conditionHandlers: Record<ConditionImpl['type'], ConditionHandler> = {
   BlockValue,
-  AlwaysFalse,
   TaskStatus,
   TimeAbsolute,
   TimeElapsed,
