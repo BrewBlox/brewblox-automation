f55d7061a74e68852b810f547a6fa12997fd1f15
Bob Steers <steers.bob@gmail.com>
2020-06-09 14:29:00 +0200

    add tests for transitions / error results

--- test/processor.test.ts
+++ test/processor.test.ts
@@ -3,11 +3,17 @@ import { v4 as uid } from 'uuid';
 
 import { getHandler } from '../src/handlers';
 import { nextUpdateResult } from '../src/processor';
-import { AutomationAction, AutomationCondition, AutomationProcess, AutomationStepResult } from '../src/types';
+import {
+  AutomationAction,
+  AutomationCondition,
+  AutomationProcess,
+  AutomationStepResult,
+  AutomationTransition,
+} from '../src/types';
 
-const mockPrepare = jest.fn().mockResolvedValue(undefined);
-const mockApply = jest.fn().mockResolvedValue(undefined);
-const mockCheck = jest.fn().mockResolvedValue(true);
+const mockPrepare = jest.fn();
+const mockApply = jest.fn();
+const mockCheck = jest.fn();
 
 const mockGetHandler = (getHandler as jest.MockedFunction<any>);
 
@@ -15,11 +21,17 @@ jest.mock('../src/handlers');
 
 type PartialResult = Partial<AutomationStepResult>;
 
-describe('Generate next update results', () => {
+describe('Generate next update results (happy flow)', () => {
   beforeEach(() => {
-    mockPrepare.mockClear();
-    mockApply.mockClear();
-    mockCheck.mockClear();
+    mockPrepare
+      .mockResolvedValue(undefined)
+      .mockClear();
+    mockApply
+      .mockResolvedValue(undefined)
+      .mockClear();
+    mockCheck
+      .mockResolvedValue(true)
+      .mockClear();
     mockGetHandler
       .mockReturnValue({
         prepare: mockPrepare,
@@ -79,8 +91,7 @@ describe('Generate next update results', () => {
       status: 'Finished',
     });
 
-    results.push(await nextUpdateResult(proc));
-    expect(last(results)).toBeNull();
+    expect(await nextUpdateResult(proc)).toBeNull();
 
     expect(mockPrepare.mock.calls.length).toBe(0);
     expect(mockApply.mock.calls.length).toBe(0);
@@ -232,5 +243,174 @@ describe('Generate next update results', () => {
         activeResult: proc.results[proc.results.length - 2],
       },
     ]);
+  });
+
+  it('Should evaluate transitions', async () => {
+    const stepId = uid();
+    const results: AutomationStepResult[] = [];
+    const condition: AutomationCondition = {
+      id: uid(),
+      enabled: true,
+      title: 'condition',
+      impl: {
+        type: 'TaskStatus',
+        ref: '',
+        resetStatus: null,
+        status: 'Finished',
+      },
+    };
+    const fullTransition: AutomationTransition = {
+      id: uid(),
+      enabled: true,
+      next: false,
+      conditions: [condition],
+    };
+    const emptyTransition: AutomationTransition = {
+      id: uid(),
+      enabled: true,
+      next: stepId,
+      conditions: [],
+    };
+    const proc: AutomationProcess = {
+      id: uid(),
+      title: 'Test process',
+      steps: [{
+        id: stepId,
+        title: 'steppy',
+        preconditions: [],
+        actions: [],
+        transitions: [
+          fullTransition,
+          emptyTransition,
+        ],
+      }],
+      results,
+    };
+
+    // full transition should first evaluate false
+    // empty transition then restarts the step
+    // full transition then ends the process
+    mockCheck
+      .mockReturnValueOnce(false)
+      .mockReturnValue(true);
+
+    // phase -> created
+    results.push(await nextUpdateResult(proc));
+    // phase -> preconditions
+    results.push(await nextUpdateResult(proc));
+    // phase -> actions
+    results.push(await nextUpdateResult(proc));
+    // phase -> transitions
+    results.push(await nextUpdateResult(proc));
+    expect(last(results)).toMatchObject<PartialResult>({
+      phase: 'Transitions',
+      status: 'Active',
+    });
+
+    // empty transition restarted the step
+    results.push(await nextUpdateResult(proc));
+    expect(mockCheck.mock.calls.length).toBe(1);
+    expect(last(results)).toMatchObject<PartialResult>({
+      stepId,
+      phase: 'Created',
+      status: 'Active',
+    });
+
+    // phase -> preconditions
+    results.push(await nextUpdateResult(proc));
+    // phase -> actions
+    results.push(await nextUpdateResult(proc));
+    // phase -> transitions
+    results.push(await nextUpdateResult(proc));
+    expect(last(results)).toMatchObject<PartialResult>({
+      phase: 'Transitions',
+      status: 'Active',
+    });
+
+    // check returns true again, full transition points towards proc end
+    results.push(await nextUpdateResult(proc));
+    expect(last(results)).toMatchObject<PartialResult>({
+      phase: 'Finished',
+      status: 'Finished',
+    });
+    expect(mockCheck.mock.calls.length).toBe(2);
+
+    expect(await nextUpdateResult(proc)).toBeNull();
+  });
+});
+
+describe('Generate next update results (error flow)', () => {
+  beforeEach(() => {
+    mockPrepare
+      .mockResolvedValue(undefined)
+      .mockClear();
+    mockApply
+      .mockResolvedValue(undefined)
+      .mockClear();
+    mockCheck
+      .mockResolvedValue(true)
+      .mockClear();
+    mockGetHandler
+      .mockReturnValue({
+        prepare: mockPrepare,
+        apply: mockApply,
+        check: mockCheck,
+      });
+  });
+
+  it('Should generate error results once', async () => {
+    const stepId = uid();
+    const results: AutomationStepResult[] = [];
+    const condition: AutomationCondition = {
+      id: uid(),
+      enabled: true,
+      title: 'condition',
+      impl: {
+        type: 'TaskStatus',
+        ref: '',
+        resetStatus: null,
+        status: 'Finished',
+      },
+    };
+    const proc: AutomationProcess = {
+      id: uid(),
+      title: 'Test process',
+      steps: [{
+        id: stepId,
+        title: 'steppy',
+        preconditions: [condition],
+        actions: [],
+        transitions: [],
+      }],
+      results,
+    };
+
+    mockCheck.mockImplementation(() => {
+      throw new Error('Implementation not here. Go away.');
+    });
+
+    // phase -> created
+    results.push(await nextUpdateResult(proc));
+    // phase -> preconditions
+    results.push(await nextUpdateResult(proc));
+    // phase -> preconditions (error)
+    results.push(await nextUpdateResult(proc));
+    expect(last(results)).toMatchObject<PartialResult>({
+      phase: 'Preconditions',
+      status: 'Active',
+      error: 'Implementation not here. Go away.',
+    });
+
+    expect(await nextUpdateResult(proc)).toBeNull();
+
+    // return to normal
+    mockCheck.mockResolvedValue(true);
+
+    // business as usual
+    results.push(await nextUpdateResult(proc));
+    expect(last(results)).toMatchObject<PartialResult>({
+      phase: 'Actions',
+      status: 'Active',
+    });
   });
 });
