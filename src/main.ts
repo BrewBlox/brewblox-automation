dd62c91aeb1c6dd3260b9d416ce6afd172aab23f
Bob Steers <steers.bob@gmail.com>
2020-08-18 16:49:09 +0200

    Add interceptor for axios errors

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,6 @@
+import axios from 'axios';
+import { get } from 'lodash';
+
 import app from './app';
 import args from './args';
 import { database } from './database';
@@ -10,6 +13,20 @@ logger.info('==========Startup==========');
 database.connect();
 eventbus.connect();
 processor.start();
+
+axios
+  .interceptors
+  .response
+  .use(
+    (response) => response,
+    (e) => {
+      const err = JSON.stringify(get(e, 'response.data', e.message ?? null));
+      const url = get(e, 'response.config.url');
+      const method = get(e, 'response.config.method');
+      const status = get(e, 'response.status');
+      const msg = `[HTTP ERROR] method=${method}, url=${url}, status=${status}, response=${err}`;
+      return Promise.reject(new Error(msg));
+    });
 
 app.listen(args.port, () => {
   logger.info(`App is running at http://0.0.0.0:${args.port}/${args.name}`);
