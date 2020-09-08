659b8f6635e7583187ed006ed4b6bb1747c21721
Bob Steers <steers.bob@gmail.com>
2020-09-08 23:57:54 +0200

    Implement sandbox in subprocess

--- docker/before_build.sh
+++ docker/before_build.sh
@@ -3,5 +3,6 @@
 set -e
 pushd "$(dirname "$(readlink -f "$0")")/.." > /dev/null
 
+rm -rf docker/package*.json docker/tsconfig.json docker/src/ || true
 cp -f package*.json tsconfig.json docker/
 cp -rf src/ docker/
